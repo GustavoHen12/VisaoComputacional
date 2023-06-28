96c17badaf5d236e426cec164b407aadb444e4f9
Gustavo Henrique <gustavo.barbosa@ufpr.br>
2023-06-28 13:13:25 -0300

    ajusta deteccao de objetos

--- FINAL/object_detection_util.py
+++ FINAL/object_detection_util.py
@@ -2,42 +2,66 @@ import cv2
 import numpy as np
 from PIL import Image
 
+def isInside(x, y, area_x1, area_y1, area_x2, area_y2):
+    return (x >= area_x1 and x <= area_x2) and (y >= area_y1 and y <= area_y2)
+
 def getImageInterestArea(image, result):
     pd = result.pandas().xyxy[0]
-    cropped_image = None
-    # Encontra box 
+
+    # Encontra area da pessoa com maior confianca
+    person_x1, person_y1, person_x2, person_y2 = 0, 0, 0, 0
+    conf = 0 
     for index, row in pd.iterrows():
-        if row["class"] == 0 and row["confidence"] >= 0.5: 
-            x1 = int(row["xmin"])
-            y1 = int(row["ymin"])
-            x2 = int(row["xmax"])
-            y2 = int(row["ymax"])
-            cropped_image = image[y1:y2, x1:x2]
-            cv2.imshow("croped", cropped_image)
-    return (cropped_image, pd)
+        if row["class"] == 0 and row["confidence"] >= 0.5 and row["confidence"] >= conf:
+            conf = row["confidence"]
+            person_x1 = int(row["xmin"])
+            person_y1 = int(row["ymin"])
+            person_x2 = int(row["xmax"])
+            person_y2 = int(row["ymax"])
 
+    if(conf == 0):
+        return None
 
-    # # Encontra area da pessoa
-    # # person_x1, person_y1, person_x2, person_y2 = None, None, None, None 
-    # # for index, row in pd.iterrows():
-    # #     if row["class"] == 0 and row["confidence"] >= 0.5: 
-    # #         person_x1 = int(row["xmin"])
-    # #         person_y1 = int(row["ymin"])
-    # #         person_x2 = int(row["xmax"])
-    # #         person_y2 = int(row["ymax"])
+    # Verifica todos os objetos na região da pessoa
+    objects = []
+    objects_x1 = [person_x1]
+    objects_y1 = [person_y1]
+    objects_x2 = [person_x2]
+    objects_y2 = [person_y2]
+    for index, row in pd.iterrows():
+        x1 = int(row["xmin"])
+        y1 = int(row["ymin"])
+        x2 = int(row["xmax"])
+        y2 = int(row["ymax"])
+        heigth = y2  - y1
+        width = x2 - x1
 
-    # for index, row in pd.iterrows():
-    #     x1 = int(row["xmin"])
-    #     y1 = int(row["ymin"])
-    #     x2 = int(row["xmax"])
-    #     y2 = int(row["ymax"])
+        # Se existe um ponto do objeto no interior da area de interesse
+        if (isInside(x1, y1, person_x1, person_y1, person_x2, person_y2)):
+            objects_x1.append(x1)
+            objects_y1.append(y1)
+            objects_x2.append(x2)
+            objects_y2.append(y2)
+            objects.append(row["name"])
+        elif (isInside(x2, y2, person_x1, person_y1, person_x2, person_y2)):
+            objects_x1.append(x1)
+            objects_y1.append(y1)
+            objects_x2.append(x2)
+            objects_y2.append(y2)
+            objects.append(row["name"])
+        elif (isInside((x1+width), y1, person_x1, person_y1, person_x2, person_y2)):
+            objects_x1.append(x1)
+            objects_y1.append(y1)
+            objects_x2.append(x2)
+            objects_y2.append(y2)
+            objects.append(row["name"])
+        elif (isInside(x1, (y2+heigth), person_x1, person_y1, person_x2, person_y2)):
+            objects_x1.append(x1)
+            objects_y1.append(y1)
+            objects_x2.append(x2)
+            objects_y2.append(y2)
+            objects.append(row["name"])
 
-    #     if (x1 <= person_x2 and x1 >= person_x1) and (y1 <= person_y2 and y1 >= person_y2):
-    #         if(x2 > person_x2):
-    #             person_x2 = x2
-    #         if(y2 > person_y2):
-    #             person_y2 = person_y2
-    #         if(x2 )
-    #         cropped_image = image[y1:y2, x1:x2]
-    #         cv2.imshow("croped", cropped_image)
-    # return (cropped_image, pd)
+    # Recorta a area que possue todos os objetos de interesse
+    cropped_image = image[min(objects_y1):max(objects_y2), min(objects_x1):max(objects_x2)]    
+    return (cropped_image, objects)
--- FINAL/ta5.py
+++ FINAL/ta5.py
@@ -13,7 +13,7 @@ yolov5_dir = Path("./yolov5")
 
 # Load the YOLOv5 model for object detection
 model = torch.hub.load(str(yolov5_dir), "custom", path=str(yolov5_dir / "yolov5s.pt"), source="local")
-model.conf = 0.6
+model.conf = 0.4
 
 # Set the device (CPU or GPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,12 +28,8 @@ model_pose_estimation.eval()
 # getPersonPosition(img, model_pose_estimation)
 # cv2.waitKey(500)
 
-
-cv2.namedWindow('croped', cv2.WINDOW_NORMAL)
-cv2.namedWindow('yolo_output', cv2.WINDOW_NORMAL)
-
 # Load the video
-video = cv2.VideoCapture("./images/video_test.mp4")
+video = cv2.VideoCapture("./images/video_test4.mp4")
 frame_count = 0
 while True:
     ret, frame = video.read()
@@ -47,10 +43,11 @@ while True:
 
     # Process area in the image with a person
     interestArea = getImageInterestArea(frame, results)
-    position = getPersonPosition(frame, model_pose_estimation)
-    
-    # if  interestArea[0] != None:
-    #     position = getPersonPosition(interestArea[0], model_pose_estimation)
+
+    if (interestArea != None and (interestArea[0]).all() != None):
+        cv2.imshow("region_of_interest", interestArea[0])
+        print(interestArea[1])
+        position = getPersonPosition(interestArea[0], model_pose_estimation)
         
     
     # Display the results

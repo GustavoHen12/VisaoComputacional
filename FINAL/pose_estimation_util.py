4745f6df86f14d0c2a8b57cd77b857590d36ee46
Gustavo Henrique <gustavo.barbosa@ufpr.br>
2023-06-28 23:06:52 -0300

    retira códigos comentados e arquivos desnecessarios

--- FINAL/pose_estimation_util.py
+++ FINAL/pose_estimation_util.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms as T
 import math
-
 from pose_estimation import draw_skeleton_per_person
 
 keypoints_index = ['nose','left_eye','right_eye','left_ear','right_ear','left_shoulder','right_shoulder','left_elbow', 'right_elbow','left_wrist','right_wrist','left_hip','right_hip','left_knee', 'right_knee', 'left_ankle','right_ankle']
@@ -38,10 +37,8 @@ def draw_keypoints_per_person(img, all_keypoints, all_scores, confs, keypoint_th
                 elif (kp == keypoints_index.index('right_hip') or kp == keypoints_index.index('left_hip')):
                     color = (255, 0, 0)
                 else:
-                    # color = tuple(np.asarray(cmap(color_id[person_id])[:-1])*255)
                     color = (255, 255, 255)
                 cv2.circle(img_copy, keypoint, 5, color, -1)
-                # cv2.drawContours(img_copy, np.array[(right_hip, right_knee)], 0, (255,255,255), 2)
 
     keypoints = all_keypoints[person_id, ...]
     scores = all_scores[person_id, ...]
@@ -54,7 +51,6 @@ def draw_keypoints_per_person(img, all_keypoints, all_scores, confs, keypoint_th
     left_knee = getKeypoint(keypoints, scores, 'left_knee', keypoint_threshold)
     
     if(encontrou_pontos(getMedia(left_hip[0], right_hip[0]), getMedia(left_hip[1], right_hip[1]),getMedia(left_shoulder[0], right_shoulder[0]),getMedia(left_shoulder[1], right_shoulder[1]),getMedia(left_knee[0], right_knee[0]),getMedia(left_knee[1], right_knee[1]))):
-        # cv2.line(img_copy, (getMedia(right_hip[0], left_hip[0]),getMedia(right_hip[1], left_hip[1])), (getMedia(right_shoulder[0], left_shoulder[0]),getMedia(right_shoulder[1], left_shoulder[1])), (255,255,255), 2)
         cv2.line(img_copy, (int(getMedia(right_hip[0], left_hip[0])), int(getMedia(right_hip[1], left_hip[1]))), (int(getMedia(right_shoulder[0], left_shoulder[0])),int(getMedia(right_shoulder[1], left_shoulder[1]))), (255,255,255), 3)
         cv2.line(img_copy, (int(getMedia(right_hip[0], left_hip[0])), int(getMedia(right_hip[1], left_hip[1]))), (int(getMedia(right_knee[0], left_knee[0])),int(getMedia(right_knee[1], left_knee[1]))), (255,255,255), 3)
         cv2.line(img_copy, (int(getMedia(right_shoulder[0], left_shoulder[0])), int(getMedia(right_shoulder[1], left_shoulder[1]))), (int(getMedia(right_knee[0], left_knee[0])),int(getMedia(right_knee[1], left_knee[1]))), (255,255,255), 3)
@@ -67,34 +63,18 @@ def calcular_angulos(img, hx, hy, sx, sy, kx, ky):
     ps = (sx, sy)
     pk = (kx, ky)
     print(ph,ps,pk)
-    # print(hx, hy, sx, sy, kx, ky)
 
     if(sx == -1 or sy == -1 or hy == -1 or hx == -1 or kx == 1 or ky == -1):
        return "Undefined"
-
-    # if(kx == -1 or ky == -1):
-    #     return "sentado"
-    # if(sx == -1 or sy == -1 or hy == -1 or hx == -1):
-    #     return "em pe"
 
     # Calculando tamanho dos lados
     sk = math.sqrt((sx - kx) ** 2 + (sy - ky) ** 2)
     hk = math.sqrt((hx - kx) ** 2 + (hy - ky) ** 2)
     hs = math.sqrt((hx - sx) ** 2 + (hy - sy) ** 2)
-
     print(sk, hk, hs)
     
-    # print((hk * 2 + hs * 2 - sk ** 2) / (2 * hk * hs))
-    # print((sk * 2 + hs * 2 - hk ** 2) / (2 * sk * hs))
-    # print((sk * 2 + hk * 2 - hs ** 2) / (2 * sk * hk))
-
     rad_a = math.acos((hk ** 2 + hs ** 2 - sk ** 2) / (2 * hk * hs))
-
     angulo_a = math.degrees(rad_a)
-    # angulo_b = math.degrees(math.acos((sk ** 2 + hs ** 2 - hk ** 2) / (2 * sk * hs)))
-    # angulo_c = math.degrees(math.acos((sk ** 2 + hk ** 2 - hs ** 2) / (2 * sk * hk)))
-
-    # print(angulo_a, angulo_b, angulo_c)
     print(angulo_a)
 
     if int(angulo_a) <= 140:
@@ -117,19 +97,7 @@ def get_pose_estimated(img, all_keypoints, all_scores, confs, keypoint_threshold
       if confs[person_id]>conf_threshold:
         keypoints = all_keypoints[person_id, ...]
         scores = all_scores[person_id, ...]
-        # print(keypoints)
-        # print("Right:")
-        # right_hip = getKeypoint(keypoints, scores, 'right_hip', keypoint_threshold)
-        # right_shoulder = getKeypoint(keypoints, scores, 'right_shoulder', keypoint_threshold)
-        # right_knee = getKeypoint(keypoints, scores, 'right_knee', keypoint_threshold)
-        # print(calcular_angulos(img, right_hip[0], right_hip[1], right_shoulder[0], right_shoulder[1], right_knee[0], right_knee[1]))
-
-        # print("Left:")
-        # left_hip = getKeypoint(keypoints, scores, 'left_hip', keypoint_threshold)
-        # left_shoulder = getKeypoint(keypoints, scores, 'left_shoulder', keypoint_threshold)
-        # left_knee = getKeypoint(keypoints, scores, 'left_knee', keypoint_threshold)
-        # print(calcular_angulos(img, left_hip[0], left_hip[1], left_shoulder[0], left_shoulder[1], left_knee[0], left_knee[1]))
-
+        
         right_hip = getKeypoint(keypoints, scores, 'right_hip', keypoint_threshold)
         right_shoulder = getKeypoint(keypoints, scores, 'right_shoulder', keypoint_threshold)
         right_knee = getKeypoint(keypoints, scores, 'right_knee', keypoint_threshold)
--- FINAL/ta5.py
+++ FINAL/ta5.py
@@ -7,22 +7,6 @@ from PIL import Image
 from object_detection_util import getImageInterestArea
 from pose_estimation_util import getPersonPosition
 
-def pad_image(imageA, imageB):
-    heightA, widthA = imageA.shape[:2]
-    heightB, widthB = imageB.shape[:2]
-
-    # Calculate the padding size
-    pad_height = (heightA - heightB) // 2
-    pad_width = (widthA - widthB) // 2
-
-    # Create a black image with the size of ImageA
-    padded_image = np.zeros_like(imageA)
-
-    # Place ImageB in the center of the padded image
-    padded_image[pad_height:pad_height + heightB, pad_width:pad_width + widthB] = imageB
-
-    return padded_image
-
 # Object detection model
 # Define the path to the YOLOv5 directory
 yolov5_dir = Path("./yolov5")
@@ -40,10 +24,6 @@ model.to(device).eval()
 model_pose_estimation = torchvision.models.detection.keypointrcnn_resnet50_fpn(pretrained=True)
 model_pose_estimation.eval()
 
-# img = cv2.imread('./images/jsent.png')
-# getPersonPosition(img, model_pose_estimation)
-# cv2.waitKey(500)
-
 # Load the video
 video = cv2.VideoCapture("./images/video_test4.mp4")
 if (video.isOpened() == False): 
@@ -59,7 +39,7 @@ out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), fps, 
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (50, 50)
 fontScale = 1
-color = (255, 0, 0)
+color = (255, 255, 255)
 thickness = 2
 
 frame_count = 0
@@ -76,22 +56,18 @@ while True:
     # Process area in the image with a person
     interestArea = getImageInterestArea(frame, results)
 
+    # Find the person position on image
     position = getPersonPosition(frame, model_pose_estimation)
 
-    # if (interestArea != None and (interestArea[0]).all() != None):
-    #     cv2.imshow("region_of_interest", interestArea[0])
-    #     print(interestArea[1])
-    #     position = getPersonPosition(pad_image(frame, interestArea[0]), model_pose_estimation)
-    # outImg = pad_image(frame, position[0])
-
-    outImg = pad_image(frame, position[0])
+    # Add info text on the result frame
+    outImg = position[0]
     outImg = cv2.putText(outImg, ("Posicao: " + str(position[1])), org, font, fontScale, color, thickness, cv2.LINE_AA)
     if(interestArea != None):
       outImg = cv2.putText(outImg, ("Objetos: " + ' '.join(interestArea[1])), (50, 100), font, 0.7, color, thickness, cv2.LINE_AA)
     cv2.imshow("output", outImg)
     out.write(outImg)
 
-    # Display the results
+    # Save the results
     cv2.imwrite("yolo_output.png", np.squeeze(results.render()))
     cv2.imwrite("input.png", frame)
     cv2.imwrite("keypoints.png", position[0])
